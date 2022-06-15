1cb7dbbd1dd1557fae49918bdafe5f53ecb86e6d
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-06-16 04:09:10 +0530

    Update __init__.py

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,6 +1,6 @@
 """
-VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+MusicChatBot, Telegram Video Chat Bot
+Copyright (c) 2021  Asm Safone <https://github.com/LISA-KOREA>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by

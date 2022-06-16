3df5dd4c4bdff7087cb353b44a410433a177394e
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-06-17 04:29:12 +0530

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,21 +2,28 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With 🤗 By Lisa 🇰🇷**"
+START_TEXT = f"👋🏻 **Hello**, {USER_NAME} \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Music Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends ☺️ \n\n**Made With 🤗 By Lisa 🇰🇷**"
 HELP_TEXT = f"""
-🛠-- **Setting Up Bot**:--
+⚫-- **Setting Up Bot**:--
 
-\u2022 **Start Voice Chat In Your Group!**
+\u2022 **Start Music Chat In Your Group!**
+
 \u2022 Add Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) To Your Group!
+
 \u2022 Give Admin To Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) In Your Group!
 
-⚔️-- **Available Commands**:--
+⚫-- **Available Commands**:--
 
-\u2022 `/play` - Stream An Audio (Example /play lisa money)
+\u2022 `/play` - Stream An Audio (**Example** /play https://www.youtube.com/watch?v=dNCWe_6HAM8)
+
 \u2022 `/stream` - Stream An Video
+
 \u2022 `/pause` - Pause Current Stream
+
 \u2022 `/resume` - Resume Paused Stream
+
 \u2022 `/endstream` - End Stream & Left VC
+
 \u2022 `/restart` - Restart Bot (Sudo Only)
 """
-ABOUT_TEXT = f"💡-- **Information**:-- \n\nI will kill you 🤣💯**"
+ABOUT_TEXT = f"💡-- **INFORMATION**:-- \n\nI will kill you 🤣💯**"
